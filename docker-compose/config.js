ff0dbc330899b70b04b3fa9013c008b824e86fd6
Ray Sun <ray.sun@oneprocloud.com>
2020-10-06 20:26:01 +0800

    Update docker-compose and fix uid and password rule

--- docker-compose/config.js
+++ docker-compose/config.js
@@ -5,14 +5,14 @@ module.exports = {
     // Base DN will be o=Example,dc=example,dc=com
     // Groups base DN will be ou=Groups,o=Example,dc=example,dc=com
     // Users base DN will be ou=People,o=Example,dc=example,dc=com
-    rootDN: 'dc=example,dc=com',
-    organization: 'Example',
+    rootDN: 'dc=oneprocloud,dc=com',
+    organization: 'department',
     // Admins who can search or modify directory
     admins: [
       {
         // Bind DN will be cn=keycloak,dc=example,dc=com
-        commonName: 'keycloak',
-        password: 'keycloak',
+        commonName: 'admin',
+        password: 'seeloo7410.',
         // Can this admin modify user's password
         canModifyEntry: true,
       },
@@ -35,8 +35,8 @@ module.exports = {
   // Provider for providen account service
   provider: {
     name: 'dingtalk',
-    appKey: '__APPKEY__',
-    appSecret: '__APPSECRET__',
+    appKey: 'dingt54hxnf6b8v3nmae',
+    appSecret: '09R6wlrUqktelx79UuEZq0s7Eixpsm8dnM-u9qtU08zDrK9x41dmxgyEUpTWa1B0',
   },
   // Custom groups, base DN will be ou=CustomGroups,ou=Groups,o=Example,dc=example,dc=com
   customGroups: [
@@ -44,7 +44,7 @@ module.exports = {
       // DN will be ou=Jenkins Admins,ou=CustomGroups,ou=Groups,o=Example,dc=example,dc=com
       name: 'Jenkins Admins',
       // User with these mails will be added to the group
-      members: [ 'jenkins@example.com' ],
+      members: [ 'jenkins@oneprocloud.com' ],
     }
   ]
 }
--- docker-compose/init/01.sql
+++ docker-compose/init/01.sql
@@ -2,4 +2,8 @@ CREATE DATABASE IF NOT EXISTS vldap default charset utf8 COLLATE utf8_general_ci
 GRANT ALL PRIVILEGES ON vldap.* to "vldap"@"localhost" identified by "vldapPass";
 GRANT ALL PRIVILEGES ON vldap.* to "vldap"@"%" identified by "vldapPass";
 
+CREATE DATABASE IF NOT EXISTS keycloak default charset utf8 COLLATE utf8_general_ci;
+GRANT ALL PRIVILEGES ON keycloak.* to "keycloak"@"localhost" identified by "keycloakPass";
+GRANT ALL PRIVILEGES ON keycloak.* to "keycloak"@"%" identified by "keycloakPass";
+
 flush privileges;
