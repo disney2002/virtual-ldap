0f0f83e5d90e4f63ca1610ee612f6ca9700d7a07
Gavin Quan <coolbee2002@163.com>
2023-04-10 14:37:26 +0800

    定期启动
    1、考虑到服务稳定性，守护进程增加了定时重启

--- start.sh
+++ start.sh
@@ -2,11 +2,21 @@
 
 while true
 do
+
+  # 检查当前时间是否为凌晨4点
+  if [ "$(date +%H:%M)" == "04:00" ]
+  then
+    # 重启服务
+    echo "restarting service: $(date)"
+    pkill -f "node index.js"
+    nohup node index.js &
+  fi
+
   # 检查进程是否在运行
   if ! pgrep -f "node index.js" > /dev/null
   then
     # 进程已经退出，启动新进程
-    echo "start a new one:$(date)"
+    echo "start a new one: $(date)"
     nohup node index.js &
   fi
   sleep 60
